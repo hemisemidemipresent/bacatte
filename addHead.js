51ae582eab0949b449e26d51d72a4bac2185f60f
hnngggrrrr <57880110+hnngggrrrr@users.noreply.github.com>
2021-01-29 16:58:47 +0800

    cool

--- addHead.js
+++
@@ -1,6 +0,0 @@
-let bacatteContainer = document.getElementById('bacatte-container');
-
-head = document.createElement('img');
-head.setAttribute('src', './head.png');
-
-bacatteContainer.appendChild(head);

ae89ac70cd723b4f9311e6b08295dbbd50c0a41c
hnngggrrrr <57880110+hnngggrrrr@users.noreply.github.com>
2021-02-09 13:43:33 +0800

    420 auto update

--- js/addBody.js
+++ js/addBody.js
@@ -3,11 +3,13 @@ let count = 1;
 function addLength() {
     if (count == 69) {
         alert('nice');
+    } else if (count == 420) {
+        location.href = './bacatte.html';
     }
     let bacatteContainer = document.getElementById('bacatte-container');
 
     head = document.createElement('img');
-    head.setAttribute('src', './body.png');
+    head.setAttribute('src', './img/body.png');
 
     bacatteContainer.insertBefore(head, bacatteContainer.childNodes[2]);
 
